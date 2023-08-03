c795afc08fe4c4acf42016499905eed493a0b32b
OrkhanKarim13 <mrorkhankarimli@gmail.com>
2023-08-03 18:06:24 +0400

    Home Sliders

--- src/components/home/SliderHome.jsx
+++ src/components/home/SliderHome.jsx
@@ -1,34 +1,38 @@
 import React from "react";
 import Slider from "react-slick";
 const SliderHome = () => {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+  
   return (
     <div>
-      <Slider {...settings} className="w-10/11 h-10/10">
-        <div>
-          <h3>1</h3>
+      <Slider>
+        <div className="!flex items-center justify-center">
+          <div>
+            <div className="text-6xl font-bold">Richard Mille</div>
+            <div className="text-xl my-3 pr-3">
+            Official website of Richard Mille, the swiss watchmaking brand that combines the best in technical innovation, in architecture and in fine watchmaking ...
+            </div>
+            <div>About</div>
+          </div>
+          <img
+            src="https://e1.pxfuel.com/desktop-wallpaper/638/358/desktop-wallpaper-sell-your-richard-mille-watch-in-london-for-cash-richard-mille-watches.jpg"
+            alt="Richard Mille"
+          />
         </div>
-        <div>
-          <h3>2</h3>
+        <div className="!flex items-center justify-center h-[529px]">
+          <img
+          
+            src="https://e1.pxfuel.com/desktop-wallpaper/747/57/desktop-wallpaper-richard-mille-watches-richard-mille.jpg"
+            alt="Richard Mille"
+          />
+          <div className="pl-5">
+            <div className="text-6xl font-bold">Richard Mille</div>
+            <div className="text-xl my-3">
+            Official website of Richard Mille, the swiss watchmaking brand that combines the best in technical innovation, in architecture and in fine watchmaking ...
+            </div>
+            <div>About</div>
+          </div>
         </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        
       </Slider>
     </div>
   );
